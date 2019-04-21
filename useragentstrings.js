a4f1f7fd97cc841a7608425b8e94a0e56fbe3251
Admin <admin@Administrator.lan>
2019-04-21 22:06:11 +1000

    First commit

---
+++ useragentstrings.js
@@ -0,0 +1,50 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const fs = require('fs');
+
+generateRandomUserAgentString();
+
+async function generateRandomUserAgentString() {
+    const url = 'http://www.useragentstring.com/pages/useragentstring.php?name=All';
+    const dom = await JSDOM.fromURL(url);  // JSDOM.fromURL(url returns a promise
+    let links = dom.window.document.querySelectorAll('a');
+    let BROWSER_AGENTS = {};
+    processArray(links, processItem).then(function(result) {
+        console.log('done');
+        console.log(Object.values(BROWSER_AGENTS));
+    }, function(err) {
+        console.log(err);
+    });
+
+    async function processItem(item) {
+        if (item.href.indexOf('http://www.useragentstring.com/index.php?id=') > -1) {
+            let id = item.href.substring(item.href.indexOf('id=') + 3);
+            console.log('Processing linkid = ' + id);
+            const promise = JSDOM.fromURL(item.href)   // JSDOM.fromURL(url returns a promise
+                .then(dom => {
+                    const document = dom.window.document;
+                    const textarea = document.querySelector('#uas_textfeld');
+                    BROWSER_AGENTS[id] = textarea.textContent;
+                    fs.appendFile('useragentstrings.txt', '\n' + textarea.textContent, (err) => {
+                        if (err) throw err;
+                    });
+                    return textarea.textContent;
+                })
+                .catch(err =>
+                    console.error(err + ' from ' + item.href)
+                );
+
+            return promise;
+        }
+    }
+
+    async function processArray(array, fn) {
+        let results = [];
+        for (let i = 0; i < array.length; i++) {
+            console.log('i = ' + i + ' of ' + array.length);
+            results.push(await fn(array[i]));
+        }
+        return results;
+    }
+
+}
